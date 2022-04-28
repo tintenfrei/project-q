080c261978f61ea7e3b1024f87522aaad9a6c5b9
Cristobal <cris.ehmann@tintenfrei.com>
2022-04-28 17:39:53 +0200

    added quotes fetch function & changed title

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let bringQuoteBtn = document.getElementById("quote-btn");
 
 let authorSection = document.getElementById("author");
 let quoteSection = document.getElementById("quote");
+
+
+fetch('https://quote-garden.herokuapp.com/api/v3/quotes')
+.then(response => response.json()) // one liner or no block of code
+.then(quotes => console.log(quotes))
+
