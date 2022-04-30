63e68d2d88276f0f9e117190bda5b841542365b0
José López Hernández <pepe@ucol.mx>
2022-04-30 15:43:18 +0200

    José: added DeepL API to script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 let e = document.getElementById('languageSelector');
-
-function getSelectValue(languageSelector) {
-    pick_ = document.getElementById(languageSelector).value;
-    localStorage.pick_ = pick_;
-    pick_ = localStorage.getItem("pick_");
-    location.reload();
-}
+let targetLanguage = e.options[e.selectedIndex].value;
 
 let textToDeep = document.getElementById("quote-text").innerHTML;
 
 
-fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${textToDeep}&target_lang=${getSelectValue}`)
+fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${textToDeep}&target_lang=${targetLanguage}`)
     .then(response => response.json())
     .then(response => {
         return document.getElementById("targetText").innerHTML = response.translations[0].text;
