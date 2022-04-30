8b2dfd4f74be6d276c248306b9669b2002ea15ed
Cristobal <cris.ehmann@tintenfrei.com>
2022-04-30 15:15:32 +0200

    ultimos cambios

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const quoteText = document.getElementById('quote-text'),
       quoteAuthor = document.getElementById('quote-author'),
       genQuoteBtn = document.getElementById('gen-quote-btn');
 
-
-fetch('https://api.quotable.io/random')
+function randomQuote(){
+    fetch('https://api.quotable.io/random')
     .then(response => response.json()) 
     .then(data => {
-        console.log(data.content)
-    })
+        quoteText.textContent = data.content;
+        quoteTags.textContent = data.tags;
+        quoteAuthor.textContent = '-- ${data.author}';
+    });
+}
+
+randomQuote();
+genQuoteBtn.addEventListener('click', () => {
+    randomQuote();
+});
