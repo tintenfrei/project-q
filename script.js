fdec3748148ef03a1ffc08974dd2654182d68ad5
Cristobal <cris.ehmann@tintenfrei.com>
2022-05-02 19:24:26 +0200

    Co-authored-by: José López Hernández <JozLH@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,25 +5,13 @@
 // let options = [{ value: "de", label: "German" }, { value: "es", label: "Spanish" }]
 
 //  the getTranslations() on option select
-call
 
 const quoteText = document.getElementById('quote-text'),
     quoteTags = document.getElementById('quote-tags'),
     quoteAuthor = document.getElementsByClassName('quote-author'),
     genQuoteBtn = document.getElementById('gen-quote-btn');
 
-console.log(quoteAuthor)
-let targetlanguage = "de";
-
-
-const getTranslations = function(text, language) {
-    fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${text}&target_lang=${language}`)
-        .then(response => response.json())
-        .then(response => {
-            return document.getElementById("targetText").innerHTML = response.translations[0].text;
-        })
-};
-
+    let targetlanguage = "de";
 
 function randomQuote() {
     fetch('https://api.quotable.io/random')
@@ -40,8 +28,17 @@ function randomQuote() {
 randomQuote();
 genQuoteBtn.addEventListener('click', () => {
     randomQuote()
-    getTranslations('Hello World!', 'de');
+    getTranslations('quoteText', 'de');
 });
+
+
+const getTranslations = function(text, language) {
+    fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${text}&target_lang=${language}`)
+        .then(response => response.json())
+        .then(response => {
+            return document.getElementById("targetText").innerHTML = response.translations[0].text;
+        })
+};
 
 // Trying to make the currently selected language in the drop menu apply to the fetch func below to fill the target language (targetlanguage variable)
 // let e = document.getElementById('languageSelector');
