a44ac59f0581f8ec8112b0814cc149240afac3ee
Cristobal <cris.ehmann@tintenfrei.com>
2022-05-03 13:19:25 +0200

    connected apis
    Co-authored-by: José López Hernández <JozLH@users.noreply.github.com>
    Co-authored-by: abed <abedhamami42@gmail.com>

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 // fix the author name issue (through generating the quote cards from the script tag )
 // generate the options from here (the script tag)
 // make an array of objects like the one below and loop over it to generate the option tags
-// let options = [{ value: "de", label: "German" }, { value: "es", label: "Spanish" }]
+let options = [{ value: "es", label: "Spanish" },{ value: "de", label: "German" }, 
+               { value: "fr", label: "French" }, { value: "cs", label: "Czech" },{ value: "el", label: "El" },]
+               
 
 //  the getTranslations() on option select
 
@@ -12,6 +14,14 @@ const quoteText = document.getElementById('quote-text'),
     genQuoteBtn = document.getElementById('gen-quote-btn');
 
     let targetlanguage = "de";
+
+const getTranslations = function(text, language) {
+    fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${text}&target_lang=${language}`)
+        .then(response => response.json())
+        .then(response => {
+        return document.getElementById("targetText").innerHTML = response.translations[0].text;
+            })
+    };
 
 function randomQuote() {
     fetch('https://api.quotable.io/random')
@@ -25,21 +35,18 @@ function randomQuote() {
         .then(data => getTranslations(data.content, 'es')); // add a (then) with a default translation 
 }
 
+console.log(quoteText.textContent)
+
 randomQuote();
+
 genQuoteBtn.addEventListener('click', () => {
     randomQuote()
-    getTranslations('quoteText', 'de');
 });
 
 
-const getTranslations = function(text, language) {
-    fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${text}&target_lang=${language}`)
-        .then(response => response.json())
-        .then(response => {
-            return document.getElementById("targetText").innerHTML = response.translations[0].text;
-        })
-};
 
-// Trying to make the currently selected language in the drop menu apply to the fetch func below to fill the target language (targetlanguage variable)
-// let e = document.getElementById('languageSelector');
-// let targetLanguage = e.options[e.selectedIndex].value;
+
+let select = document.getElementById('languageSelector');
+
+
+select.addEventListener("change", () => getTranslations(quoteText.textContent, select[select.selectedIndex].value));
