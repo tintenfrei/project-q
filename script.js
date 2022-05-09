56b269034f6c04dec071b0b03471bc17564b3db5
Cristobal <cris.ehmann@tintenfrei.com>
2022-05-09 19:00:10 +0200

    commenting

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
+// declaring 4 variables to document
+
 const quoteText = document.getElementById('quote-text'),
     quoteTags = document.getElementById('quote-tags'),
     quoteAuthor = document.querySelectorAll('.quote-author'),
     genQuoteBtn = document.getElementById('gen-quote-btn');
-let targetlanguage = "de";
+
+    //retrieving and storing the language of the actual user because every user starts with their won language (works on Chrome, Brave and Firefox so far, apparently not yet on Safari)
 let userLang = navigator.language || navigator.userLanguage;
+
+// ONLY CREATING AND STORING TRANSLATIONS FUNCTION
+// storing in a const the translation api from deepl.com with two parameters: text to be translated (otext) and the target language (language), additionally to the two parameters we are translating
+// Quote of the day
 const getTranslations = function(otext, language) {
     fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&text=${otext}@Quote%20of%20the%20day&target_lang=${language}`)
         .then(response => response.json())
         .then(response => {
+            // storing in ttext variable the result fromt the api translation including otext and "Quote of the Day"
             let ttext = response.translations[0].text;
             const h1text =  ttext.split("@")[1]
+            // splitting with split method thaat converts a string into an array; pushing the result into document according to IDs
             document.getElementById("targetText").innerHTML = ttext.split("@")[0];
             document.getElementById("headerForTranslation").innerHTML = h1text;
         })
 };
-
+// CREATING DIFFERENT TARGET LANGUAGES
+// creating empty array for the different language options
 let options = [];
+// declaring the variable that stores the different languages on document
 var selectBox = document.getElementById('languageSelector');
-
+// creating function for retrieving possible target languages offered by deepl.com
 function presentLangOptions() {
     fetch(`https://api-free.deepl.com/v2/languages?auth_key=8b66872d-f900-ca3f-dd80-cda50dfab6f6%3Afx&type=target`)
         .then(response => response.json())
+        // function that sets variable options equal to the received languages as an array
         .then(response => options = response)
+        // function with a looping through all languages
         .then(() => {
             for (let i = 0; i < options.length; i++) {
                 var option = options[i];
+                // variable that stores true if language looped is equal to the default language of user
                 let isSelected = (option.language === userLang.toUpperCase()) ? true : false;
+                // method that adds new objects into the selectBox array to the dropdown list and checks default language of user
                 selectBox.options.add(new Option(option.name, option.language, isSelected, isSelected));
             }
         })
-        .then(selectBox[selectBox.selectedIndex] = 5)
 }
-console.log(options)
+// calling the function to retrieve possible languages
 presentLangOptions();
-
+// RETRIEVING QUOTES FROM API
 function randomQuote() {
     fetch('https://api.quotable.io/random')
         .then(response => response.json())
         .then(data => {
             quoteText.textContent = data.content;
             quoteTags.textContent = data.tags;
-            // console.log(quoteAuthor[0])
+            // we use the author two times with a html class
             quoteAuthor[0].textContent = `-- ${data.author}`;
             quoteAuthor[1].textContent = `-- ${data.author}`;
             return data;
         })
         .then(data => getTranslations(data.content, userLang))
-        .then(console.log(selectBox)); // add a (then) with a default translation
+        
 }
-console.log(quoteText.textContent)
+// calling random quote
 randomQuote();
+// event handler for getting new quote on document
 genQuoteBtn.addEventListener('click', () => {
     randomQuote()
 });
+// event handler for the languages dropdown to translated
 selectBox.addEventListener("change", () => getTranslations(quoteText.textContent, selectBox[selectBox.selectedIndex].value));
 
-
-// let text = targetText.innerHTML;
-// const myH1Array = text.split("@");
-// let word = myH1Array[1];
-// document.getElementById("headerForTranslation").innerHTML = word[1];
-
-// let splitHeader = targetText.split("@");
-// document.getElementById("headerForTranslation").innerHTML = splitHeader[1];
-
-
-// let splitHeaderh1 = targetText.innerHTML.split("@");
-// document.getElementById("headerForTranslation").innerHTML = splitHeaderh1[1];
-
-// let splitHeader2 = targetText.split("@");
-// document.getElementById("headerForTranslation") = splitHeader2[1];
-
-// let splitHeaderrr = targetText.innerHTML.split("@");
-// document.getElementById("headerForTranslation").textContent = splitHeaderrr[1];
